7f0cd32e35bfa5c3229b8eac719e47c9f54a7ce8
Marco Ambu <marco.ambu@gmail.com>
2012-09-24 19:25:48 +0100

    Added unit test framework.

---
+++ src/test/java/hudson/plugins/xshell/XShellTest.java
@@ -0,0 +1,15 @@
+package hudson.plugins.xshell;
+
+import org.jvnet.hudson.test.HudsonTestCase;
+
+/**
+ *
+ * @author marco.ambu
+ */
+public class XShellTest extends HudsonTestCase {
+
+  public void testRegex() throws Exception {
+//    assertEquals("ciao", "ciao2");
+  }
+}
+
